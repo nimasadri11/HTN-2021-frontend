341348c7c9d0e6189a76523c41a0036eafb02e8a
Caroline Huang <caroline.huang38@gmail.com>
2021-09-18 10:16:00 -0400

    implemented qr scanner

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import HomeScreen from "./app/screens/HomeScreen";
+import CodeScanner from "./app/screens/CodeScanner";
 
 export default function App() {
-  return <HomeScreen />;
+  return <CodeScanner />;
 }
